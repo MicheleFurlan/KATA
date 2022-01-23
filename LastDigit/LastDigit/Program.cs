e81351a52ab16be90fa94ecf1e897862920487dc
MicheleFurlan <michele.u.a.furlan@gmail.com>
2022-01-23 14:46:41 +0000

    LastDigit - suboptimal

---
+++ LastDigit/LastDigit/Program.cs
@@ -0,0 +1,39 @@
+﻿// https://www.codewars.com/kata/584dee06fe9c9aef810001e8/train/csharp
+using System;
+using System.Collections.Generic;
+using System.Numerics; 
+
+namespace Kata
+{
+    public class GreenNumbers
+    {
+
+        public static BigInteger Get(int v)
+        {
+            //List<int> list1 = new List<int>();
+            int n = 0;
+            double result = 0;
+
+            //for (int i = 0; list1.Count <= v; i++)
+            for (int i = 0; n <= v; i++)
+            {
+
+                double s = Math.Pow(i, 2);
+                int il = i.ToString().Length;
+                var ss = s.ToString();
+                string s1 = ss.Substring(ss.Length - il, il);
+
+                if (s1 == i.ToString())
+                {
+                    result = i;
+                    n++;
+                }
+            }
+
+            return (BigInteger)result;// list1[list1.Count - 1];
+        }
+
+        static void Main() {}
+
+    }
+}
---
+++ LastDigit/LastDigitTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+using Kata;
+
+namespace LastDigitTest
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new BigInteger(1), GreenNumbers.Get(1), "for n = 1");
+            Assert.AreEqual(new BigInteger(5), GreenNumbers.Get(2), "for n = 2");
+            Assert.AreEqual(new BigInteger(6), GreenNumbers.Get(3), "for n = 3");
+            Assert.AreEqual(new BigInteger(25), GreenNumbers.Get(4), "for n = 4");
+        }
+
+        [TestMethod]
+        public void BiggerTests()
+        {
+            Assert.AreEqual(new BigInteger(2890625), GreenNumbers.Get(12), "for n = 12");
+            //Assert.AreEqual(new BigInteger(7109376), GreenNumbers.Get(13), "for n = 13");
+        }
+
+        [TestMethod]
+        public void AdvancedTests()
+        {
+            Assert.AreEqual(BigInteger.Parse("6188999442576576769103890995893380022607743740081787109376"), GreenNumbers.Get(100), "for n = 100");
+            Assert.AreEqual(BigInteger.Parse("9580863811000557423423230896109004106619977392256259918212890625"), GreenNumbers.Get(110), "for n = 110");
+        }
+    }
+}
